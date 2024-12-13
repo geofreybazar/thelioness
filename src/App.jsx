f8540e0222c4c53d22439f0f77cee0889bb6f094
geofreybazar <geofreybazar@gmail.com>
2024-12-13 10:19:15 +0800

    fix code wallet structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,7 @@ import LandingPage from "./pages/LandingPage";
 import Stake from "./pages/Stake";
 import NavBar from "./components/Universalcomponent/NavBar.jsx";
 import Footer from "./components/Universalcomponent/Footer.jsx";
-
-import { useMemo } from 'react';
-import { ConnectionProvider, WalletProvider,  } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
-import {WalletModalProvider,} from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
-import '@solana/wallet-adapter-react-ui/styles.css';
+import AppWalletProvider from "./components/AppWalletProvider.jsx";
 
 
 const queryClient = new QueryClient();
@@ -19,31 +12,24 @@ const queryClient = new QueryClient();
 function App() {
 
   const navigate = useNavigate();
-  const network = WalletAdapterNetwork.Devnet;
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-  const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
   const handleStakeNow = () => {
     navigate('/stake')
   };
 
   return (
     <QueryClientProvider client={queryClient}>
-        <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider wallets={wallets} autoConnect>
-                <WalletModalProvider>
-                  <NavBar handleStakeNow={handleStakeNow}/>
-                    <Routes>
-                      <Route path='/' 
-                        element={<LandingPage handleStakeNow={handleStakeNow}/>}
-                      />
-                      <Route path='/stake' 
-                        element={<Stake/>}
-                      />
-                    </Routes>
-                  <Footer/>      
-                </WalletModalProvider>
-            </WalletProvider>
-        </ConnectionProvider>       
+      <AppWalletProvider>
+        <NavBar handleStakeNow={handleStakeNow}/>
+          <Routes>
+            <Route path='/' 
+              element={<LandingPage handleStakeNow={handleStakeNow}/>}
+            />
+            <Route path='/stake' 
+              element={<Stake/>}
+            />
+          </Routes>
+        <Footer/>      
+       </AppWalletProvider>
     </QueryClientProvider>
   )
 }
