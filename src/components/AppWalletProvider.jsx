d3a5299e7c540b36da5bd665b80d02f4b3acd2ab
JC <175778997+jcpunzalan123@users.noreply.github.com>
2024-12-18 17:16:21 +0800

    Added multiple solana wallets in connect wallet.

--- src/components/AppWalletProvider.jsx
+++ src/components/AppWalletProvider.jsx
@@ -1,7 +1,12 @@
 import { useMemo } from 'react';
 import { ConnectionProvider, WalletProvider,  } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { 
+  PhantomWalletAdapter,
+  LedgerWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
 import {WalletModalProvider,} from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 import '@solana/wallet-adapter-react-ui/styles.css';
@@ -9,7 +14,13 @@ import '@solana/wallet-adapter-react-ui/styles.css';
 function AppWalletProvider({children}) {
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-  const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
+  const wallets = useMemo(() => [
+                  new PhantomWalletAdapter(),
+                  new LedgerWalletAdapter(),
+                  new SolflareWalletAdapter({ network }),
+                  new TorusWalletAdapter(),
+                ],
+                [network]);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
