5b5406f18de7638963bf2cf9f337862589ad51e2
geofreybazar <geofreybazar@gmail.com>
2024-12-21 10:09:50 +0800

    add some ui on stake dashboard

--- src/components/Stake/MonitoringCards.jsx
+++ src/components/Stake/MonitoringCards.jsx
@@ -18,17 +18,15 @@ function MonitoringCards() {
   ];
 
 
-
-
   return (
     <div>
       <div className='w-full flex justify-center gap-12'>
         {monitoringValues.map((field,index) => (
-        <div key={index} className='p-5 rounded-2xl text-white text-2xl font-semibold grid grid-cols-2 items-center bg-backgroundImg w-96'>
-            <div >{field.title}:</div>
+        <div key={index} className='p-5 rounded-2xl text-white grid grid-cols-2 items-center bg-backgroundImg w-96'>
+            <div className="text-2xl">{field.title}:</div>
             <div className='flex gap-5 justify-center'>
               <img className='w-16' src={logo} alt="elsa" />
-              {field.value}
+              <p className="font-bold text-3xl">{field.value}</p> 
             </div>       
         </div>
       ))}
--- src/components/Stake/StakingCard/Stake.jsx
+++ src/components/Stake/StakingCard/Stake.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { TextField } from '@mui/material';
 import ButtonComponent from '../../ReusableComponent/ButtonComponent.jsx';
 import logo from '../../../assets/logo.svg'
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 
 const AprPerDays = [
   {
@@ -22,58 +23,77 @@ const AprPerDays = [
 ];
 
 function Stake() {
-  const [isActive, setIsActive] = useState(1)
+  const [isActive, setIsActive] = useState(1);
+  const [selectedApr, setSelectedApr] = useState("5%");
   const activeStyle = "bg-backgroundImg text-main p-5 cursor-pointer rounded-xl text-xl flex flex-col items-center";
   const inactiveStyle = "bg-gray-200 text-black p-5 cursor-pointer rounded-xl text-xl flex flex-col items-center";
 
   return (
-    <div className='font-semibold flex flex-col gap-5'>
-      <div className='font-poppins flex items-center gap-5'>
-        <div className='w-28'>
-          <img src={logo} alt="elsa" />
+    <div className='flex gap-8'>
+      <div className='w-full font-semibold flex flex-col gap-5 p-5'>
+        <div className='font-poppins flex items-center gap-5'>
+          <div className='w-28'>
+            <img src={logo} alt="elsa" />
+          </div>
+          <div className='text-2xl'>
+            Elsa
+          </div>
         </div>
-        <div className='text-2xl'>
-          Elsa
-        </div>
-      </div>
-      <div className='flex flex-col gap-5'>
-        Staking Period:
-        <div className='flex gap-6'>
-          {AprPerDays.map((field,index) => (
-            <div key={index} 
-            className={isActive === field.value ? activeStyle : inactiveStyle}
-            onClick={() => setIsActive(field.value)}
-            >
-              <div>
-                {field.percentage} 
+        <div className='flex flex-col gap-5'>
+          Staking Period:
+          <div className='flex gap-6 justify-center'>
+            {AprPerDays.map((field,index) => (
+              <div key={index} 
+              className={isActive === field.value ? activeStyle : inactiveStyle}
+              onClick={() => {setIsActive(field.value); setSelectedApr(field.percentage)}}
+              >
+                <div>
+                  {field.percentage} 
+                </div>
+                <div>
+                  {field.duration} days
+                </div>              
               </div>
-              <div>
-                {field.duration} days
-              </div>              
+            ))}
+          </div>
+        </div>
+        <div className='flex items-center justify-between'>
+          <p>Wallet Balance:</p> 
+          <div className='flex items-center'>
+            <div className='w-12 '>
+              <img src={logo} alt="elsa"/>
             </div>
-          ))}
+            <div>
+                0.00
+            </div>
+          </div> 
+        </div>
+        <div className='flex flex-col'>
+          <p>Amount</p>
+          <TextField
+            size="small"
+            type='number'
+          />
+        </div>
+        <ButtonComponent>
+          Stake
+        </ButtonComponent>
+      </div>
+
+      <div className='w-full border flex flex-col rounded-2xl'>
+        <div className='h-2/4 bg-backgroundImg rounded-t-2xl p-5 text-main flex flex-col justify-center'>
+            <p className='text-xl font-semibold'>Fixed APR</p>
+            <p className='text-5xl font-semibold'><TrendingUpIcon sx={{ fontSize: 50 }}/>{selectedApr}</p>
+            <p>estimated earning: 0.00 usd</p>        
+        </div>
+        <div className='h-2/4 p-5 flex flex-col gap-3'>
+        <p>Daily Real-Time Reward:</p>
+        <p>Subscription Time:</p>
+        <p>Interest Accrual Time:</p>
+        <p>Interest Payment:</p>
         </div>
       </div>
-      <div className='flex items-center justify-between'>
-        <p>Wallet Balance:</p> 
-        <div className='flex items-center'>
-          <div className='w-12 '>
-            <img src={logo} alt="elsa"/>
-          </div>
-          <div>
-              0.00
-          </div>
-        </div> 
-      </div>
-      <div className='flex flex-col'>
-        <p>Amount</p>
-        <TextField
-          size="small"
-        />
-      </div>
-      <ButtonComponent>
-        Stake
-      </ButtonComponent>
+      
     </div>
   )
 }
